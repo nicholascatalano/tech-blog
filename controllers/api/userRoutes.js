74a6161f93d82d46fd0bfd1a30fba9e662e59a93
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 16:54:28 -0500

    adds userRoutes file, adds post route to create new User session

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// route to create new User session
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      (req.session.userId = userData.id),
+        (req.session.username = userData.username),
+        (req.session.loggedIn = true),
+        res.json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
