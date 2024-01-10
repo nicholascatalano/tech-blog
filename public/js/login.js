bb294818d477c34753e55989d1c122cb0e65485b
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 17:14:03 -0500

    adds login javascript

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard"); // reloads page to dashboard if logged in successfully
+    } else {
+      alert("Failed to login!");
+    }
+  }
+};
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
