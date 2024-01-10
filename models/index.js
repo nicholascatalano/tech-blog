f531acc64f3fcb9fbeff5668294bc9b9a337bd0d
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 15:45:02 -0500

    adds relationships between Models, exports

--- models/index.js
+++ models/index.js
@@ -1,3 +1,35 @@
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
+
+// User has many Post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+// User has many Comment
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+// Post has many Comment
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+// Post belongs to User
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// Comment belongs to User
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
