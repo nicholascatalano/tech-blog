aee7f9ec5b9b3fb833d30237e28a76d1f04eba73
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 18:18:15 -0500

    adds post userRoute to check user's input for login if it exists in db, and creates new session

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,4 +19,41 @@ router.post("/", async (req, res) => {
   }
 });
 
+// post route to check users login username and pass, if it exists in db create new session
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect user or password, please try again" });
+      return;
+    }
+
+    const password = await userData.checkPassword(req.body.password);
+
+    if (!password) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
+
+      res.json({ userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json({ message: "No user account found!" });
+  }
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
 const homeRoutes = require("./homeRoutes.js");
 // const dashboardRoutes = require("./dashboardRoutes.js");
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 
 // router.use("/dashboard", dashboardRoutes);
 
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
