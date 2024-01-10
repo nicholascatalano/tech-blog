c7e7d680f9837fb2738afab4a67d7e0f6896a9de
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 18:19:01 -0500

    adds post route to logout user from session

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -56,4 +56,15 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// post route to logout user
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
