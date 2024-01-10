ea7025573f819755bcb77ccc10768473c8fab77b
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 16:19:53 -0500

    adjust seeding js

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,22 +7,17 @@ const postData = require("./postData.json");
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true, // checks that password is hashed using hooks in User model
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
     returning: true,
   });
 
-  const users = await User.findAll();
-
-  // create array of post obj by mapping over the postData array & adding userId property to each post obj, which is obtained by finding the user with the corresponding 'username' property in the 'users' array fetched above
-
-  const posts = postData.map((post) => ({
-    ...post,
-    userId: users.find((user) => user.username === post.username).id,
-  }));
-
-  // pass array of post objs created above into bulkCreate method to seed Post model with postData.json data
-  await Post.bulkCreate(posts);
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
