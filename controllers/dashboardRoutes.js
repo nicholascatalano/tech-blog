89808767789f5416b6831c6f0247301e1e07aa5f
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-10 18:22:59 -0500

    adds GET dashboardRoutes to findAll user posts and render them to dashboard handlebar page

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
 const { Post } = require("../models/");
+const withAuth = require("../utils/auth");
+
+// GET route, withAuth, to find all user posts
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    // deserialize postData
+    const posts = postData.map((post) =>
+      post.get({
+        plain: true,
+      })
+    );
+    res.render("allPostsAdmin", { layout: "dashboard", posts });
+  } catch (err) {
+    // if withAuth fails or user has no posts, redirect to login
+    res.redirect("login");
+  }
+});
+
+module.exports = router;
